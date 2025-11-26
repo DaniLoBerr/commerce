4ea1333a93a522d8178383d670a07263f70e41c8
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2025-11-26 15:22:21 +0000

    feat(comments): add comment view to urls.py

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("bid/<int:listing_id>", views.bid, name="bid"),
     path("close", views.close, name="close"),
+    path("comment/<int:listing_id>", views.comment, name="comment"),
     path("listing/<int:id>", views.listing, name="listing"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
